ceccc1421d1e84e98800a23121e8182bc80ca47a
정상미 <w2321@e-mirim.hs.kr>
2024-10-26 22:06:54 +0900

    자바 스크립트 - 14

---
+++ scripts/3-13(module).js
@@ -0,0 +1,19 @@
+// function log(message) {
+//     console.log(message);
+// }
+
+// function error(message) {
+//     console.error(message);
+// }
+
+// export - 내보내다
+export function log(message) {
+    console.log(message);
+}
+
+export function error(message) {
+    console.error(message);
+}
+
+// 여기서는 무조건 상수 변수만 지정해 줘야 함
+export const PI = 3.14;
---
+++ scripts/3-13(ref).js
@@ -0,0 +1,3 @@
+import {log} from "/scripts/3-13(module).js";
+
+console.log("REF 에서 출력하는 메세지");
